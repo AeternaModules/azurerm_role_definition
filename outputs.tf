f9e04ca9aeb2b2bb4a2640e23ba9fdb6a06a9153
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:44 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,32 @@ output "role_definitions" {
   description = "All role_definition resources"
   value       = azurerm_role_definition.role_definitions
 }
+output "role_definitions_assignable_scopes" {
+  description = "List of assignable_scopes values across all role_definitions"
+  value       = [for k, v in azurerm_role_definition.role_definitions : v.assignable_scopes]
+}
+output "role_definitions_description" {
+  description = "List of description values across all role_definitions"
+  value       = [for k, v in azurerm_role_definition.role_definitions : v.description]
+}
+output "role_definitions_name" {
+  description = "List of name values across all role_definitions"
+  value       = [for k, v in azurerm_role_definition.role_definitions : v.name]
+}
+output "role_definitions_permissions" {
+  description = "List of permissions values across all role_definitions"
+  value       = [for k, v in azurerm_role_definition.role_definitions : v.permissions]
+}
+output "role_definitions_role_definition_id" {
+  description = "List of role_definition_id values across all role_definitions"
+  value       = [for k, v in azurerm_role_definition.role_definitions : v.role_definition_id]
+}
+output "role_definitions_role_definition_resource_id" {
+  description = "List of role_definition_resource_id values across all role_definitions"
+  value       = [for k, v in azurerm_role_definition.role_definitions : v.role_definition_resource_id]
+}
+output "role_definitions_scope" {
+  description = "List of scope values across all role_definitions"
+  value       = [for k, v in azurerm_role_definition.role_definitions : v.scope]
+}
 
