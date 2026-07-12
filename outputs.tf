85036f82097a5fef5147809cf613c501e3b79108
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:02 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "role_definitions" {
-  description = "All role_definition resources"
-  value       = azurerm_role_definition.role_definitions
-}
 output "role_definitions_assignable_scopes" {
-  description = "List of assignable_scopes values across all role_definitions"
-  value       = [for k, v in azurerm_role_definition.role_definitions : v.assignable_scopes]
+  description = "Map of assignable_scopes values across all role_definitions, keyed the same as var.role_definitions"
+  value       = { for k, v in azurerm_role_definition.role_definitions : k => v.assignable_scopes }
 }
 output "role_definitions_description" {
-  description = "List of description values across all role_definitions"
-  value       = [for k, v in azurerm_role_definition.role_definitions : v.description]
+  description = "Map of description values across all role_definitions, keyed the same as var.role_definitions"
+  value       = { for k, v in azurerm_role_definition.role_definitions : k => v.description }
 }
 output "role_definitions_name" {
-  description = "List of name values across all role_definitions"
-  value       = [for k, v in azurerm_role_definition.role_definitions : v.name]
+  description = "Map of name values across all role_definitions, keyed the same as var.role_definitions"
+  value       = { for k, v in azurerm_role_definition.role_definitions : k => v.name }
 }
 output "role_definitions_permissions" {
-  description = "List of permissions values across all role_definitions"
-  value       = [for k, v in azurerm_role_definition.role_definitions : v.permissions]
+  description = "Map of permissions values across all role_definitions, keyed the same as var.role_definitions"
+  value       = { for k, v in azurerm_role_definition.role_definitions : k => v.permissions }
 }
 output "role_definitions_role_definition_id" {
-  description = "List of role_definition_id values across all role_definitions"
-  value       = [for k, v in azurerm_role_definition.role_definitions : v.role_definition_id]
+  description = "Map of role_definition_id values across all role_definitions, keyed the same as var.role_definitions"
+  value       = { for k, v in azurerm_role_definition.role_definitions : k => v.role_definition_id }
 }
 output "role_definitions_role_definition_resource_id" {
-  description = "List of role_definition_resource_id values across all role_definitions"
-  value       = [for k, v in azurerm_role_definition.role_definitions : v.role_definition_resource_id]
+  description = "Map of role_definition_resource_id values across all role_definitions, keyed the same as var.role_definitions"
+  value       = { for k, v in azurerm_role_definition.role_definitions : k => v.role_definition_resource_id }
 }
 output "role_definitions_scope" {
-  description = "List of scope values across all role_definitions"
-  value       = [for k, v in azurerm_role_definition.role_definitions : v.scope]
+  description = "Map of scope values across all role_definitions, keyed the same as var.role_definitions"
+  value       = { for k, v in azurerm_role_definition.role_definitions : k => v.scope }
 }
 
